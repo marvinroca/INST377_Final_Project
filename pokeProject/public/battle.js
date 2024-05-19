87ff6d73211f2de946c141411601158fa11ca96f
Navieran <ashleytien01@gmail.com>
2024-05-19 17:05:01 -0400

    Fixed the table a bit
    Updates when fetch call occurs. Formatted slightly

--- pokeProject/public/battle.js
+++ pokeProject/public/battle.js
@@ -15,6 +15,9 @@ async function fetchPokemon() {
 
     // Send battle data to the server
     await sendDataToServer(pokemon1, pokemon2);
+    
+    // Update table 
+    await makeTable();
 }
 
 /* added new */
@@ -156,6 +159,11 @@ async function makeTable() {
     const battleData = await getBattles();
     console.log(battleData);
 
+    
+    // Clear previous table
+    const container = document.getElementById('battleTable');
+    container.innerHTML = '';
+
     const table = document.createElement('table');
     table.border = 1;
 
@@ -191,9 +199,5 @@ async function makeTable() {
         });
     });
 
-    // Append table to the container
-    const container = document.getElementById('battleTable');
     container.appendChild(table);
 }
-
-window.onload = makeTable();
